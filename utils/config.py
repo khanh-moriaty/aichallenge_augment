0cbb697292affdfaaa192bac2e6b2e602f99a660
Khanh Moriaty <19520624@gm.uit.edu.vn>
2020-08-08 19:15:17 +0700

    Fixed typos.

--- utils/config.py
+++ utils/config.py
@@ -4,7 +4,7 @@ import random
 INPUT_DIR = './dataset/'
 OUTPUT_DIR = './output/'
 ITERS = 5
-CPU_PROCESSES = 20
+CPU_PROCESSES = 1
 
 HASH_BASE = 10**9 + 7
 
@@ -16,9 +16,9 @@ def getSeed(a=None):
     return int(time.time() * 10**8) % HASH_BASE
 
 
-APPLY_FLIP = False
+APPLY_FLIP = True
 
-APPLY_SHADE = False
+APPLY_SHADE = True
 SHADE_RANGE = (0, .5)
 SHADE_SIZE = 0.6
 SHADE_SHIFT_MIN = -0.15
@@ -26,7 +26,7 @@ SHADE_SHIFT_MAX = +0.15
 SHADE_GAMMA_MIN = .75
 SHADE_GAMMA_MAX = 2
 
-APPLY_HSV = False
+APPLY_HSV = True
 HUE_MIN = -0.05
 HUE_MAX = +0.05
 SATURATION_MIN = -0.15
@@ -34,7 +34,7 @@ SATURATION_MAX = +0.15
 HSV_VALUE_MIN = -0.15
 HSV_VALUE_MAX = +0.15
 
-APPLY_CUT = False
+APPLY_CUT = True
 CUT_WIDTH_MIN = 0.5
 CUT_WIDTH_MAX = 1.0
 CUT_HEIGHT_MIN = 0.5
